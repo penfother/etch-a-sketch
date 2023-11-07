ad2088d9ec30f9f3fe8fbec428c2be70fcb931d3
penfother <filip.pendic2@gmail.com>
2023-11-07 20:34:19 +0100

    table is visible with numbered cells of columns and rows for each cell

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
-//creates a div table for etch-a-sketch with defined number of columns and rows 
-let rowNum = 4;
-let columnNum = 4;
-let table = [];
+//creates a table of divs with defined number of rows and columns
+function setTable(numberOfRows, numberOfColumns, tableDOM) {
 
-for (let o = 0; o < rowNum; o++) {
-    let generateRow = [];
+    //generates usable rows for table
+    let table = [];
+    for (let o = 1; o < numberOfRows + 1; o++) {
+        let generateRow = [];
+        let rowStorage = document.createElement("div");
+        tableDOM.appendChild(rowStorage);
+        rowStorage.setAttribute("class", "row" + o);
+        for (let i = 1; i < numberOfColumns + 1; i++) {
+            let storage;
+            storage = document.createElement("div");
+            storage.setAttribute("class", "row" + o + " " + "column" + i);
+            rowStorage.appendChild(storage)
+            generateRow["column" + i]= storage;
 
-    for (let i = 0; i < columnNum; i++) {
-        generateRow[i]= i;
+        };
+        table["row" + o] = generateRow;    
     };
-
-    table["row" + o] = generateRow;    
+    return table;
 };
 
-console.log(table);
-console.log(table["row3"]);
+const tableSpread = document.body.appendChild(document.createElement("div"));
+tableSpread.id = "tableSpread";
+console.log(setTable(4,4, tableSpread));
