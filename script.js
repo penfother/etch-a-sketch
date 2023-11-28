6216a9771f16f67d45cd77c28e7a123b47317a85
penfother <filip.pendic2@gmail.com>
2023-11-28 21:29:14 +0100

    added clear button, added prompt for etch-a-sketch size when user initialises the program

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
-//creates table with rows and columns of divs
-function setTable(numberOfRows, numberOfColumns, tableDOM) {
+let tableSpread=null;
+
+//creates table with rows of square divs
+function setTable(num, tableDOM) {
     let table = [];
-    let c=1;
-    for (let o = 1; o < numberOfRows + 1; o++) {
+    for (let o = 0; o < num; o++) {
         let generateRow = [];
         let rowStorage = document.createElement("div");
         tableDOM.appendChild(rowStorage);
-        rowStorage.setAttribute("class", "row" + o + " " + "row");
-        for (let i = 1; i < numberOfColumns + 1; i++) {
-            let storage;
-            storage = document.createElement("div");
-            storage.setAttribute("class", "column" + i + " " + "column");
-            storage.setAttribute("class", "square");
-            storage.setAttribute("id", "square" + c);
-            c++;
-            rowStorage.appendChild(storage)
+        rowStorage.setAttribute("class","row");
+        for (let i = 0; i < num; i++) {
+            let storage=document.createElement("div");
+            storage.setAttribute("class", "square")
+            rowStorage.appendChild(storage);
             generateRow["column" + i]= storage;
         };
         table["row" + o] = generateRow;    
@@ -22,26 +19,48 @@ function setTable(numberOfRows, numberOfColumns, tableDOM) {
     return table;
 };
 
-const tableSpread = document.body.appendChild(document.createElement("div"));
-tableSpread.setAttribute("class", "tableSpread");
-
 //button that asks for user input to create new table
-let a=window.prompt("How big do you want your etch-a-sketch to be?");
+function startEtch() {
+    if (tableSpread!=null) {
+        tableSpread.remove()
+    };
+    tableSpread = document.body.appendChild(document.createElement("div"));
+    tableSpread.setAttribute("class", "tableSpread");
+    var userInput=window.prompt("Enter the desired size of the Etch-a-Sketch",100);
+    let gridNum=parseInt(userInput);
+    if (gridNum>100) {
+        gridNum=100;
+    };
+    setTable(gridNum, tableSpread);
+    //randomly change color with each mouseover
+    function changeColor(item) {
+        let randomColor=Math.floor(Math.random()*16777215).toString(16);
+        return "#" + randomColor;
+    };
+    //enables mouseover to color the background of div
+    let squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+        square.addEventListener("mouseover", ()=> {
+            square.style.backgroundColor = changeColor();
+        });
+    });
 
-setTable(a,a, tableSpread);
-
-//randomly change color with each mouseover
-function changeColor(item) {
-    let randomColor=Math.floor(Math.random()*16777215).toString(16);
-    return "#" + randomColor;
+    //clears the grid of colored divs on a button press
+   
 };
 
-//enables mouseover to color the background of div
-let squares = document.querySelectorAll(".square");
-squares.forEach((square) => {
-    square.addEventListener("mouseover", ()=> {
-        square.style.backgroundColor = changeColor();
+let clearButton=document.getElementById("clear");
+clearButton.onclick= function clear() {
+    let squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+        square.style.backgroundColor= "white";
     });
-});
+};
+
+
+
+
+
+
 
 
